b802d7464e0d4b5c1ff4a336fbae40c1298ce0d5
ahmedyasser20 <ahmedyasser200211@gmail.com>
2023-11-28 21:36:06 +0200

    Process Struct

--- code/DataStructures/DataStructures.h
+++ code/DataStructures/DataStructures.h
@@ -1,4 +1,14 @@
 #ifndef DATA_STRUCTURES_HEADER
 #define DATA_STRUCTURES_HEADER
 
+struct Process
+{
+    int id;         //ID for Each Process Unique
+    int ArriveTime; //Arrive time of process (when i arrive to cpu or OS see it) 
+    int Runtime;    //run time  (the time that OS give the process)
+    int Priority;   //Priority of Processes
+};
+
+
+
 #endif
