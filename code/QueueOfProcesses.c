78df368006ba059f790e21922c084830b5b95184
ahmedyasser20 <ahmedyasser200211@gmail.com>
2023-12-02 19:45:57 +0200

    QueueOfProcesses Comment added

--- code/QueueOfProcesses.c
+++ code/QueueOfProcesses.c
@@ -1,6 +1,14 @@
 #include "QueueOfProcesses.h"
 #include <stdlib.h>
 
+/*
+    Function name: NewNode
+    Description: Creating Queue Node with process and set the next pointer to NULL
+    Input: struct Process 
+    Output: struct QueueProcessNode* (Pointer) 
+
+*/
+
 
 struct QueueProcessNode* NewNode(struct Process item)
 {
@@ -11,12 +19,30 @@ struct QueueProcessNode* NewNode(struct Process item)
          return temp;
 }
 
+/*
+    Function name: CreateQueueOfProcess
+    Description: allocate memory for Queue (head and tail pointers) then return Pointer
+                 Ref. to it
+    Input: void
+    Output: struct Queue* 
+
+*/
+
 struct Queue* CreateQueueOfProcess()
 {
     struct Queue* q=(struct Queue*)malloc(sizeof(struct Queue));
     q->head=q->tail=NULL;
     return q;
 }
+
+/*
+    Function name: Push
+    Description: Creating a Node using Process by call CreateQueueOfProcess() then add
+                 it at the end of queue using struct Queue Pointer
+    Input: struct Queue * , struct Process 
+    Output: void 
+
+*/
 
 void Push(struct Queue *q,struct Process item){
     struct QueueProcessNode * temp=NewNode(item);
@@ -27,6 +53,15 @@ void Push(struct Queue *q,struct Process item){
     q->tail->next=temp;
     q->tail=temp;
 }
+
+
+/*
+    Function name: Pop
+    Description: remove First Node in Queue (Head=Head->next)
+    Input: struct Queue * 
+    Output: void 
+
+*/
 
 void Pop(struct Queue *q){
     if(q->head==NULL)
--- code/QueueOfProcesses.h
+++ code/QueueOfProcesses.h
@@ -3,6 +3,13 @@
 
 #include "DataStructures.h"
 
+/*
+    Structure of Processes Queue
+        =>Head, tail Pointer to Nodes
+        =>Node {Process , pointer to the next processes}
+
+*/
+
 struct QueueProcessNode
 {
     struct Process key;
@@ -14,11 +21,47 @@ struct Queue
     struct QueueProcessNode *head,*tail;
 };
 
+/*
+    Function name: NewNode
+    Description: Creating Queue Node with process and set the next pointer to NULL
+    Input: struct Process 
+    Output: struct QueueProcessNode* (Pointer) 
+
+*/
+
+
 struct QueueProcessNode* NewNode(struct Process item);
+
+
+/*
+    Function name: CreateQueueOfProcess
+    Description: allocate memory for Queue (head and tail pointers) then return Pointer
+                 Ref. to it
+    Input: void
+    Output: struct Queue* 
+
+*/
 
 struct Queue* CreateQueueOfProcess(); 
 
+/*
+    Function name: Push
+    Description: Creating a Node using Process by call CreateQueueOfProcess() then add
+                 it at the end of queue using struct Queue Pointer
+    Input: struct Queue * , struct Process 
+    Output: void 
+
+*/
+
 void Push(struct Queue *q,struct Process Item);
+
+/*
+    Function name: Pop
+    Description: remove First Node in Queue (Head=Head->next)
+    Input: struct Queue * 
+    Output: void 
+
+*/
 
 void Pop(struct Queue *q);
 
