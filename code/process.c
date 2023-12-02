9c651d487dd045f4cf5efbeddccfe1777c75c46f
ahmedyasser20 <ahmedyasser200211@gmail.com>
2023-12-02 19:25:10 +0200

    Added Function ForkProcess()

--- code/process.c
+++ code/process.c
@@ -6,14 +6,18 @@ int remainingtime;
 int main(int agrc, char * argv[])
 {
     initClk();
-    
+    int start =getClk();
+    printf("Process here start %d\n",start);
+    remainingtime=getClk()+5;
+   
     //TODO it needs to get the remaining time from somewhere
     //remainingtime = ??;
-    while (remainingtime > 0)
+    while (remainingtime > start)
     {
-        // remainingtime = ??;
+       start=getClk();
     }
-    
+    printf("Process here end %d\n",start);
+    kill(getppid(),SIGUSR2);
     destroyClk(false);
     
     return 0;
--- code/scheduler.c
+++ code/scheduler.c
@@ -5,24 +5,44 @@ int QueueKey;
 struct Queue *ReadyQueue;
 bool Genetor=true;
 
-void SignalHandlerProcessesEnd(int sig){
+bool isRunning=false;
+
+void SignalHandlerGentorEnd(int sig){
     printf("Flag");
     Genetor=false;
 }
 
+void SignalHandlerProcessesEnd(int sig){
+    isRunning=false;
+}
+
+void ForkProcess(int x){
+    int processid=fork();
+        printf("Forked %d\n",processid);
+    if(processid==0){
+
+         char para=x;
+         execl("process.out",&para,NULL);
+    }
+    if(processid<0){
+        printf("Error while creating Scheduler");
+        exit(-1);
+    }
+}
 
 void HPF(){
-    for (int i = 0; i < 5; i++)
-    {
-        printf("HPF \n");
-        sleep(5);
+    if(!isRunning && ReadyQueue->head!=NULL){
+        isRunning=true;
+        puts("HERE in HPF\n");
+        ForkProcess(ReadyQueue->head->key.Runtime);
+        Pop(ReadyQueue);
     }
-    
 }
 
 int main(int argc , char*argv[]){
     printf("ID %d\n",getpid());
-    signal(SIGUSR1,SignalHandlerProcessesEnd);
+    signal(SIGUSR1,SignalHandlerGentorEnd);
+    signal(SIGUSR2,SignalHandlerProcessesEnd);
     initClk();
 
     ReadyQueue=CreateQueueOfProcess();
@@ -33,10 +53,11 @@ int main(int argc , char*argv[]){
         struct MsgGeneratorScheduler temp;    
         int Rev=msgrcv(QueueKey,&temp,sizeof(temp.p),0,IPC_NOWAIT);
         if(Rev!=-1){
-           printf("ID %d\n",temp.p.id);
+           Push(ReadyQueue,temp.p); 
         }
-    }while(Genetor);
-    HPF();
+        HPF();    
+    }while(Genetor || ReadyQueue->head!=NULL);
+    
     
     //destroyClk(true);
     return 0;
